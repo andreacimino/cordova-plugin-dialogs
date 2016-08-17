42c93cdfe1bc6d7163a3466e86a74d1ab219134c
Andrea Cimino <andreacimino@gmail.com>
2016-08-17 12:07:45 +0200

    Fixed syntax

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -316,7 +316,7 @@ public class Notification extends CordovaPlugin {
                                     }
                                     callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
                                 }
-                     })
+                        });
                 }
 
                 else {
